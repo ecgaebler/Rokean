e7b77f86c9810c28aabf86ad716496831e12143f
Curious Cuttlefisch <curious.cuttlefisch@frostfeather.net>
2021-05-28 10:14:47 -0700

    Created Rokean generator cpp file

---
+++ generate_rokean.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+bool LoadSyllables(std::string file_name, std::vector<std::string> syllables) {
+    std::ifstream file(file_name.c_str());
+    if(!file) {
+        std:cerr << "ERROR: could not find syllables file: " << file_name << '\n';
+        return false;
+    }
+    clog << "Successfully opened syllables file: " << file_name << '\n'; 
+
+    //read lines into vector of syllables
+    std::string current_line;
+    while (std::getline(file, current_line)) {
+        clog << current_line << '\n'; 
+        //if(current_line.size() > 0) syllables.push_back(current_line);
+        syllables.push_back(current_line);
+    }
+
+    file.close();
+    return true;
+}
+
+
+int main() {
+    std::vector<std::string> syllables;
+
+    bool file_read = LoadSyllables("rokean_syllables", syllables);
+
+    if(file_read) {
+        //for(std::string & line : syllables) {
+        //    std::cout << line << '\n';
+        //}
+        std:cout << syllables.size() << '\n';
+    }
+}
