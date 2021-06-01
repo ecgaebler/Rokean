b6f76286d6affdc72fc50802339d90bd81eccde9
Curious Cuttlefisch <curious.cuttlefisch@frostfeather.net>
2021-05-31 21:00:24 -0700

    binary search passed my tests and seems to work properly.

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # Rokean
 Another language project, this time influenced by Korean phonetics.
-I wanted to get some practice with C/C++ while revisiting my interest in linguistics. When completed, this program should generate the vocabulary for a new agglutinative synthetic language, given a list of English words and a list of syllables to build words from. While I chose to base Rokean in part on Korean, I designed this program to be more flexible and reuseable than my previous project, Katanese. 
-use command "make" to compile from source.
+I wanted to get some practice with C/C++ while revisiting my interest in linguistics. When completed, this program should generate the vocabulary for a new agglutinative synthetic language, given a list of English words and a .csv file with syllables to build words from, and the relative frequencies of those syllables. While I chose to base Rokean in part on Korean, I designed this program to be more flexible and reuseable than my previous project, Katanese. 
+
+Note: this program requires a file named "syllables_with_freq.csv" in the same directory. Each line of this file must follow this format:
+<syllable>,<frequency>
+where <syllable> is a string representing a syllable, and <frequency> is an integer representing the desired relative frequency of that syllable (the values are arbitrary, but must be positive integers). If the "frequency" value for one syllable is twice that of another, it will be twice as likely to occur when words are generated.
+
+Use command "make" to compile from source.
--- rokean_generator.cpp
+++ rokean_generator.cpp
@@ -1,6 +1,6 @@
 /*******************
  * author: ecgaebler
- * version: 0.3
+ * version: 0.4
  * date: 2021.05.31
  * 
  * This program requires a file "syllables_with_freq.csv" with a specific
@@ -15,6 +15,12 @@
  * Note that spaces and tabs in the csv file may be treated as alphanumeric 
  * characters. Be careful how you use them or you might get whitespace in your 
  * syllables.
+ * 
+ * Planned Features That I Might Add At Some Point:
+ * - Program can use a file with only the syllables, and generate appropriate 
+ *   frequencies following a zipf distribution.
+ * - Program can take an arbitrary file as input and determine if it includes
+ *   only syllables, or syllables with associated frequencies.
  *******************/
 #include <iostream>
 #include <fstream>
@@ -61,7 +67,8 @@ bool SplitLine(std::string& line, std::string& syllable, int& frequency) {
     }
     
     if(substrings.size() != 2) {
-        std::cerr << "ERROR: " << substrings.size() << " substrings in line. Should be 2.";
+        std::cerr << "ERROR: " << substrings.size() << " substrings in line. "
+            "Should be 2.\n";
         return false;
     }
 
@@ -81,10 +88,12 @@ bool SplitLine(std::string& line, std::string& syllable, int& frequency) {
 }
 
 
-// This function randomly picks a syllable weighted by the syllable's frequency
-std::string GenerateSyllable(std::vector<std::string>& syllables, std::vector<int>& freq_weights) {
+// Randomly picks a syllable weighted by the syllable's frequency
+std::string GenerateSyllable(std::vector<std::string>& syllables, 
+                             std::vector<int>& freq_weights) {
     if(freq_weights.size() != syllables.size()) {
-        std::cerr << "ERROR: number of syllables does not match number of frequencies.\n";
+        std::cerr << "ERROR: number of syllables does not match number of "
+            "frequencies.\n";
         return "";
     }
     if(freq_weights.size() < 2) {
@@ -92,24 +101,34 @@ std::string GenerateSyllable(std::vector<std::string>& syllables, std::vector<in
         return "";
     }
 
-    // Binary search for 
+    // Generate random int, then do BinarySearch.
 }
 
 
-int BinarySearch(std::vector<int>& nums, int& target) {
-    int l = 0; 
-    int r = nums.size() - 1;
+// Returns the index of the vector with the smallest value greater than target.
+int BinarySearch(std::vector<int>& nums, int target) {
+    if(nums.size() == 0) {
+        std::cerr << "ERROR: cannot search empty vector\n";
+        return -1;
+    }
+
+    int l_idx = 0; 
+    int r_idx = nums.size() - 1;
     int mid;
-    while(l + 1 < r) {
-        mid = l + (r - l) / 2; // This looks awkward, but avoids int overflow.
+    while(l_idx + 1 < r_idx) {
+        mid = l_idx + (r_idx - l_idx) / 2; // looks awkward but avoids overflow.
         if(nums.at(mid) < target) {
-            l = mid;
+            l_idx = mid;
         } else {
-            r = mid;
+            r_idx = mid;
         }
     }
-    
 
+    if(target <= nums.at(l_idx)) {
+        return l_idx;
+    } else {
+        return r_idx;
+    }
 }
 
 
@@ -136,6 +155,17 @@ int main() {
             }
         }
 
-
+        // Testing BinarySearch
+        std::vector<int> nums;
+        nums.push_back(3);
+        nums.push_back(7);
+        nums.push_back(13);
+        std::clog << "t=0; goal=0, actual=" << BinarySearch(nums, 0) << '\n';
+        std::clog << "t=3; goal=0, actual=" << BinarySearch(nums, 3) << '\n';
+        std::clog << "t=4; goal=1, actual=" << BinarySearch(nums, 4) << '\n';
+        std::clog << "t=7; goal=1, actual=" << BinarySearch(nums, 7) << '\n';
+        std::clog << "t=8; goal=2, actual=" << BinarySearch(nums, 8) << '\n';
+        std::clog << "t=13; goal=2, actual=" << BinarySearch(nums, 13) << '\n';
+        std::clog << "t=42; goal=2, actual=" << BinarySearch(nums, 42) << '\n';
     } 
 }
